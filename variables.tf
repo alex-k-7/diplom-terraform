57c13f083161668a37e4f1d66b3c31c44c44da96
Aleksandr Kosogorskii <k.alex.spb@gmail.com>
2022-06-06 17:08:07 +0300

    add variables

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "OA_TOKEN" {
+    type = string
+}
+
+variable "CLOUD_ID" {
+    type = string
+}
+
+variable "FOLDER_ID" {
+    type = string
+}
+
+variable "ZONE" {
+    type = string
+}
