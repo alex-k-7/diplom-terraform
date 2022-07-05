8c018d5312c263c6b570e8df760dccad8e01a744
Aleksandr Kosogorskii <k.alex.spb@gmail.com>
2022-07-05 13:11:26 +0300

    add grafana balancer

--- main.tf
+++ main.tf
@@ -88,3 +88,24 @@ resource "yandex_lb_network_load_balancer" "lb-1" {
         }
     }
 }
+
+resource "yandex_lb_network_load_balancer" "lb-2" {
+    name = "grafana-lb"   
+    listener {
+        name = "grafana-listener"
+        port = 8080
+        target_port = 30902
+        external_address_spec {
+            ip_version = "ipv4"
+        }
+    }
+    attached_target_group {
+        target_group_id = "${yandex_lb_target_group.tg-1.id}"
+        healthcheck {
+            name = "app_check"
+            http_options {
+                port = 30902
+            }
+        }
+    }
+}
