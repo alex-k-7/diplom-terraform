d63559bdc437a923ab92db17a921ce6112a6fce9
Aleksandr Kosogorskii <k.alex.spb@gmail.com>
2022-07-05 15:47:43 +0300

    delete lb-2

--- main.tf
+++ main.tf
@@ -88,24 +88,3 @@ resource "yandex_lb_network_load_balancer" "lb-1" {
         }
     }
 }
-
-/*resource "yandex_lb_network_load_balancer" "lb-2" {
-    name = "grafana-lb"   
-    listener {
-        name = "grafana-listener"
-        port = 8080
-        target_port = 30902
-        external_address_spec {
-            ip_version = "ipv4"
-        }
-    }
-    attached_target_group {
-        target_group_id = "${yandex_lb_target_group.tg-1.id}"
-        healthcheck {
-            name = "http"
-            http_options {
-                port = 30902
-            }
-        }
-    }
-}*/
